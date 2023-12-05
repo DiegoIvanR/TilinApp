6c7f9126c4cb2a560e557ed4ee0092dd6f14f328
ññ <>
2023-12-05 16:51:27 -0600

    Initial Commit

---
+++ TilinApp/TilinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TilinAppApp.swift
+//  TilinApp
+//
+//  Created by gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TilinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
